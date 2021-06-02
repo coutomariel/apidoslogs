34ea937a0742b3c82eb5d026898fcc56fd47d56d
Mariel Couto <coutomariel@gmail.com>
2021-06-02 14:35:35 -0300

    Exemplo de utilização de logs na aplicação.

--- src/main/java/br/com/coutomairel/apidoslogs/ApidoslogsApplication.java
+++ src/main/java/br/com/coutomairel/apidoslogs/ApidoslogsApplication.java
@@ -2,6 +2,9 @@ package br.com.coutomairel.apidoslogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApidoslogsApplication {
@@ -10,5 +13,27 @@ public class ApidoslogsApplication {
 		SpringApplication.run(ApidoslogsApplication.class, args);
 
 	}
+	
+	@RestController
+	@RequestMapping("/home")
+	public class Initial {
+
+		private final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Initial.class);
+
+		@GetMapping
+		public String hello() {
+			logger.info("Log de informação");
+			logger.warn("Log de aviso, algo está errado ou faltando! cuidado!");
+			logger.error("Log de erro, algo de errado aconteceu!");
+			logger.debug("Log de depuração, contém informações mais refinadas, que são mais úteis para depurar um app");
+			logger.trace("Log de rastreabilidade, contém informações mais refinadas do que o DEBUG");
+
+			// exibindo variáveis no log
+			logger.info("Proposta documento={} salário={} criada com sucesso!", "9915183128", 7999.00);
+			return "Olá mundo!";
+		}
+
+	}
+
 
 }
