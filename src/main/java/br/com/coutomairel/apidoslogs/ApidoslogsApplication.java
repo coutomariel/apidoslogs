eaf76bb3155188aa444641580d0bc83dfe97f3aa
Mariel Couto <coutomariel@gmail.com>
2021-06-02 14:34:20 -0300

    First commit

---
+++ src/main/java/br/com/coutomairel/apidoslogs/ApidoslogsApplication.java
@@ -0,0 +1,14 @@
+package br.com.coutomairel.apidoslogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidoslogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidoslogsApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/br/com/coutomairel/apidoslogs/ApidoslogsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coutomairel.apidoslogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidoslogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
